c36cf261f3990d43c297f31929c09ee3c370e021
Emrullah Enis Çetinkaya <eniscetinkaya951@gmail.com>
2025-03-02 19:50:43 +0300

    License file has been created

--- src/calculator_app/Main.java
+++ src/calculator_app/Main.java
@@ -1,3 +1,14 @@
+/*
+ * Bu dosya, GNU Genel Kamu Lisansı (GPL) sürüm 3 veya daha üstü altında lisanslanmıştır.
+ * Detaylı bilgi için lütfen LICENSE.txt dosyasına bakınız.
+ *
+ * Bu program, faydalı olacağı umuduyla dağıtılmaktadır,
+ * ancak HERHANGİ BİR GARANTİ VERİLMEMEKTEDİR;
+ * hatta SATILABİLİRLİK veya BELİRLİ BİR AMACA UYGUNLUK garantisi bile verilmez.
+ *
+ * GNU Genel Kamu Lisansı'nın bir kopyasını LICENSE.txt dosyasında bulabilirsiniz.
+ */
+
 package calculator_app;
 
 public class Main {
